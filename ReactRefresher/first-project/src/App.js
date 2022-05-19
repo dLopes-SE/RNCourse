3961fb68639841c5fee476a097b8b05dc4611fe1
dlopes <dylananlopes@gmail.com>
2022-05-19 21:38:59 +0100

    finish 1st react project

---
+++ ReactRefresher/first-project/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the course' },
+    { id: 'cg3', text: 'Help other students in Q&A' },
+  ]);
+
+  const addNewGoalHandler = newGoal => {
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+  };
+
+  return ( 
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
+    </div>
+  );
+}
+
+export default App;
