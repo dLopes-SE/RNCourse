d08f8f61b9823fdfbb96f932d3700272cb615c0c
dlopes <dylananlopes@gmail.com>
2022-07-13 20:34:23 +0100

    Switched ScrollView to FlatList

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -5,8 +5,8 @@ import {
   Text,
   View,
   Button,
-  TextInput,
-  ScrollView,
+  FlatList,
+  TextInput
 } from "react-native";
 
 export default function App() {
@@ -20,7 +20,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      {text: enteredGoalText, id: Math.random().toString()},
     ]);
   }
 
@@ -35,13 +35,20 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalList}>
-        <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList 
+          data={courseGoals}
+          renderItem={itemData => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}>
+        </FlatList>
       </View>
     </View>
   );
