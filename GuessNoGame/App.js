af18c864decebe9e8f37343e2a2ca2945e02435f
dlopes <dylananlopes@gmail.com>
2022-08-07 14:49:19 +0100

    app background

--- GuessNoGame/App.js
+++ GuessNoGame/App.js
@@ -1,11 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-      <StartGameScreen />
-  );
+	return (
+		<LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+			<ImageBackground
+				style={styles.rootScreen}
+				source={require("./assets/images/background.png")}
+				resizeMode="cover"
+				imageStyle={styles.backgroundImage}
+			>
+				<StartGameScreen />
+			</ImageBackground>
+		</LinearGradient>
+	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.15,
+	},
+});
--- GuessNoGame/screens/StartGameScreen.js
+++ GuessNoGame/screens/StartGameScreen.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: "#4e0329",
+		backgroundColor: "#3b021f",
 		borderRadius: 12,
 		shadowColor: "black",
 		shadowOffset: { width: 0, height: 2 },
